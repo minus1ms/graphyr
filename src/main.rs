60634d3d9472612aa41475190117ecd896d24442
minus1ms <qwertyytpl1612@gmail.com>
2025-01-09 23:12:47 +0100

    floem gut

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use floem::{kurbo::Stroke, prelude::*};
+
 fn main() {
-    println!("a");
+    floem::launch(graphyr_view);
+}
+
+fn graphyr_view() -> impl IntoView {
+    h_stack((
+        v_stack((create_cell(), create_cell())).style(|s| s.width_full()),
+        create_configuration(),
+    ))
+    .style(|s| s.width_full())
+}
+
+fn create_cell() -> Container {
+    container(text("a")).style(|s| {
+        s.border(Stroke::new(1.0))
+            .height_full()
+            .items_center()
+            .justify_center()
+    })
+}
+
+fn create_configuration() -> Container {
+    container(text("Configuration:")).style(|s| s.padding(10).border(Stroke::new(2.0)))
 }
