72fb1fcefe263e5d39fd50fa90adb37a280e34e2
minus1ms <qwertyytpl1612@gmail.com>
2025-01-13 13:45:50 +0100

    progress

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,75 @@ use theme::MyTheme;
 
 mod theme;
 
+struct Cell {
+    title: RwSignal<String>,
+    table: Option<Table>,
+}
+
+impl Cell {
+    fn new(table: Option<Table>) -> Self {
+        Self {
+            title: RwSignal::new(String::new()),
+            table,
+        }
+    }
+
+    fn into_view(&self, my_theme: MyTheme) -> impl View {
+        let has_table = self.table.is_some();
+        v_stack((self.create_cell_title(my_theme.clone()), {
+            let res: Box<dyn View> = if let Some(table) = &self.table {
+                Box::new(table.into_view(my_theme.clone()))
+            } else {
+                Box::new(empty())
+            };
+            res
+        }))
+        .style(move |s| {
+            if has_table { s.gap(15) } else { s }
+                .border(Stroke::new(1.0))
+                .items_center()
+                .justify_center()
+                .hover(|s| s.background(my_theme.background_hovered))
+                .size_full()
+                .padding(15)
+        })
+    }
+
+    fn create_cell_title(&self, my_theme: MyTheme) -> TextInput {
+        let text = self.title.clone();
+        text_input(text).style(move |s| {
+            let font_size = 12.;
+            s.border_color(Color::TRANSPARENT)
+                .background(my_theme.background)
+                .max_width_full()
+                .width({
+                    let mut text_layout = TextLayout::new();
+                    text_layout.set_text(
+                        &text.get(),
+                        AttrsList::new(Attrs::new().font_size(font_size)),
+                    );
+                    text_layout.size().width + 12.
+                })
+                .font_size(font_size)
+        })
+    }
+}
+
+struct Table {
+    cells: Vec<Vec<Cell>>,
+}
+
+impl Table {
+    fn into_view(&self, my_theme: MyTheme) -> impl View {
+        v_stack_from_iter((0..self.cells.len()).map(|i| {
+            let row = &self.cells[i];
+            h_stack_from_iter((0..row.len()).map(|i| row[i].into_view(my_theme.clone())))
+                .style(|s| s.size_full())
+        }))
+        .style(|s| s.size_full())
+    }
+}
+
 fn main() {
     floem::launch(graphyr_view);
 }
@@ -15,58 +84,21 @@ fn graphyr_view() -> impl IntoView {
     let my_theme = MyTheme::default();
 
     h_stack((
-        create_table(my_theme.clone(), true).style(|s| s.width_full()),
+        Cell::new(Some(Table {
+            cells: vec![vec![
+                Cell::new(Some(Table {
+                    cells: vec![vec![Cell::new(None)]],
+                })),
+                Cell::new(Some(Table {
+                    cells: vec![vec![Cell::new(None)]],
+                })),
+            ]],
+        }))
+        .into_view(my_theme.clone()),
         create_configuration(),
     ))
     .style(move |s| theme::theme(s, &my_theme))
     .style(|s| s.width_full())
-}
-
-fn create_table(my_theme: MyTheme, inner: bool) -> Stack {
-    let grid = 5;
-
-    v_stack_from_iter((0..grid).map(|_| {
-        h_stack_from_iter(
-            (0..grid).map(|_| create_cell(my_theme.clone(), inner).style(|s| s.width_full())),
-        )
-        .style(|s| s.height_full())
-    }))
-}
-
-fn create_cell(my_theme: MyTheme, table: bool) -> Stack {
-    v_stack((create_cell_title(my_theme.clone()), {
-        let res: Box<dyn View> = if table {
-            Box::new(create_table(my_theme.clone(), false))
-        } else {
-            Box::new(empty())
-        };
-        res
-    }))
-    .style(move |s| {
-        s.border(Stroke::new(1.0))
-            .items_center()
-            .justify_center()
-            .hover(|s| s.background(my_theme.background_hovered))
-    })
-}
-
-fn create_cell_title(my_theme: MyTheme) -> TextInput {
-    let text = RwSignal::new(String::new());
-    text_input(text).style(move |s| {
-        let font_size = 12.;
-        s.border_color(Color::TRANSPARENT)
-            .background(my_theme.background)
-            .max_width_full()
-            .width({
-                let mut text_layout = TextLayout::new();
-                text_layout.set_text(
-                    &text.get(),
-                    AttrsList::new(Attrs::new().font_size(font_size)),
-                );
-                text_layout.size().width + 12.
-            })
-            .font_size(font_size)
-    })
 }
 
 fn create_configuration() -> Container {
