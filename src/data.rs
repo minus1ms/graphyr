3b7be2b8ffec4ba13092cbf76106278d79ba43a1
minus1ms <qwertyytpl1612@gmail.com>
2025-01-15 12:49:57 +0100

    row and col adding skeleton

--- src/data.rs
+++ src/data.rs
@@ -1,17 +1,53 @@
+use std::iter;
+
 use floem::{
     kurbo::Stroke,
     menu::{Menu, MenuEntry, MenuItem},
     peniko::Color,
     prelude::{RwSignal, SignalGet as _, SignalUpdate as _},
+    taffy::Position,
     text::{Attrs, AttrsList, TextLayout},
     views::{
         dyn_container, empty, h_stack_from_iter, text_input, v_stack, v_stack_from_iter,
-        Decorators as _, TextInput,
+        Decorators as _, Empty, TextInput,
     },
     IntoView, View,
 };
 
 use crate::{theme::MyTheme, view_data::ViewData};
+
+#[derive(Clone)]
+pub struct CellId(Vec<usize>);
+
+impl CellId {
+    pub fn new() -> Self {
+        Self(vec![0])
+    }
+
+    pub fn current(&self) -> usize {
+        self.0[0]
+    }
+
+    pub fn next(&self) -> CellIdRef {
+        CellIdRef(&self.0[1..])
+    }
+}
+
+pub struct CellIdRef<'a>(&'a [usize]);
+
+impl<'a> CellIdRef<'a> {
+    pub fn current(&self) -> usize {
+        self.0[0]
+    }
+
+    pub fn next(&self) -> CellIdRef {
+        CellIdRef(&self.0[1..])
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+}
 
 #[derive(Clone)]
 pub struct Cell {
@@ -32,7 +68,7 @@ impl Cell {
     pub fn into_view(&self, my_theme: MyTheme) -> impl View {
         let hierarchy_depth = self.hierarchy_depth;
         let size_multiplier = (100. - (hierarchy_depth * 10) as f32) / 100.;
-        
+
         let table = self.table.clone();
         v_stack((
             self.create_cell_title(my_theme.clone(), size_multiplier),
@@ -55,13 +91,17 @@ impl Cell {
             }),
         ))
         .style(move |s| {
-            if table.get().is_some() { s.gap(15. * size_multiplier) } else { s }
-                .border(Stroke::new(1.0))
-                .items_center()
-                .justify_center()
-                .hover(|s| s.background(my_theme.background_hovered))
-                .size_full()
-                .padding(15. * size_multiplier)
+            if table.get().is_some() {
+                s.gap(15. * size_multiplier)
+            } else {
+                s
+            }
+            .border(Stroke::new(1.0))
+            .items_center()
+            .justify_center()
+            .hover(|s| s.background(my_theme.background_hovered))
+            .size_full()
+            .padding(15. * size_multiplier)
         })
         .context_menu({
             let hierarchy_depth = self.hierarchy_depth;
@@ -76,9 +116,9 @@ impl Cell {
                         },
                     )))
                 } else {
-                    res.entry(MenuEntry::Item(MenuItem::new("Remove table").action(move || {
-                        table.set(None)
-                    })))
+                    res.entry(MenuEntry::Item(
+                        MenuItem::new("Remove table").action(move || table.set(None)),
+                    ))
                 }
             }
         })
@@ -103,20 +143,21 @@ impl Cell {
         })
     }
 
-    pub fn get_inner_cell(&self, place: &[usize]) -> &Cell {
-        if place.is_empty() {
+    pub fn get_inner_cell(&self, id_ref: CellIdRef) -> &Cell {
+        if id_ref.is_empty() {
             return self;
         }
 
-        let first = place[0];
+        let first = id_ref.current();
 
         let cells = &self.table.get_untracked().unwrap().cells;
         let rows = cells.len(); // single col size
         let cols = cells[0].len(); // single row size
-
         let row = first / cols;
         let col = rows - first * row;
         println!("{row} {col}");
+
+        let rest = id_ref.next();
 
         None.unwrap()
     }
@@ -129,12 +170,59 @@ pub struct Table {
 
 impl Table {
     pub fn into_view(&self, my_theme: MyTheme) -> impl View {
-        v_stack_from_iter((0..self.cells.len()).map(|i| {
-            let row = &self.cells[i];
-            h_stack_from_iter((0..row.len()).map(|i| row[i].into_view(my_theme.clone())))
+        v_stack_from_iter(
+            iter::once(
+                // this is a column pane, first of the rows
+                Self::create_table_pane(my_theme.clone(), true).into_any(),
+            )
+            .chain((0..self.cells.len()).map(|i| {
+                let row = &self.cells[i];
+                h_stack_from_iter(
+                    iter::once(
+                        // this is a row pane, first of the columns
+                        Self::create_table_pane(my_theme.clone(), false).into_any(),
+                    )
+                    .chain((0..row.len()).map(|i| row[i].into_view(my_theme.clone()).into_any())),
+                )
                 .style(|s| s.size_full())
-        }))
+                .into_any()
+            })),
+        )
         .style(|s| s.size_full())
+    }
+
+    fn create_table_pane(my_theme: MyTheme, column: bool) -> Empty {
+        let size = 20;
+        if column {
+            empty().context_menu(|| {
+                Menu::new("")
+                    .entry(MenuEntry::SubMenu(
+                        Menu::new("Add")
+                            .entry(MenuEntry::Item(MenuItem::new("PRE")))
+                            .entry(MenuEntry::Item(MenuItem::new("POST"))),
+                    ))
+                    .entry(MenuEntry::Item(MenuItem::new("Remove")))
+            })
+        } else {
+            empty().context_menu(|| {
+                Menu::new("")
+                    .entry(MenuEntry::SubMenu(
+                        Menu::new("Add")
+                            .entry(MenuEntry::Item(MenuItem::new("PRE")))
+                            .entry(MenuEntry::Item(MenuItem::new("POST"))),
+                    ))
+                    .entry(MenuEntry::Item(MenuItem::new("Remove")))
+            })
+        }
+        .style(move |s| {
+            if column {
+                s.height(size).width_full()
+            } else {
+                s.width(size).height_full()
+            }
+            .background(my_theme.secondary_background)
+            .border(Stroke::new(1.0))
+        })
     }
 }
 
@@ -152,9 +240,9 @@ impl Data {
     }
 
     pub fn get_cell(&self, place: &ViewData) -> &Cell {
-        let cell_place = place.get_cell();
-        let first = cell_place[0];
+        let cell_id = place.get_cell();
+        let first = cell_id.current();
         assert!(first == 0);
-        self.cell.get_inner_cell(&cell_place[1..])
+        self.cell.get_inner_cell(cell_id.next())
     }
 }
--- src/view_data.rs
+++ src/view_data.rs
@@ -1,18 +1,20 @@
+use crate::data::CellId;
+
 // data used to generate the view, it uses our main centralized data storage
 #[derive(Clone)]
 pub struct ViewData {
     // it points to a cell in Data
-    displayed_cell: Vec<usize>,
+    displayed_cell: CellId,
 }
 
 impl ViewData {
     pub fn new() -> Self {
         Self {
-            displayed_cell: vec![0],
+            displayed_cell: CellId::new(),
         }
     }
 
-    pub fn get_cell(&self) -> &[usize] {
+    pub fn get_cell(&self) -> &CellId {
         &self.displayed_cell
     }
 }
